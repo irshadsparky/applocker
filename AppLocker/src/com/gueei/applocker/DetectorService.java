860e7f9282716fe722b0102ab54d110be56cab1e
omran.hesham <omran.hesham@gmail.com>
2013-09-20 22:27:45 +0000

    Jellybean update. The app uses android.permission.get_task now. android.permission.read_logs is not supported on android 4.0+

--- AppLocker/src/com/gueei/applocker/DetectorService.java
+++ AppLocker/src/com/gueei/applocker/DetectorService.java
@@ -1,3 +1,10 @@
+//References used:
+//http://stackoverflow.com/questions/2166961/determining-the-current-foreground-application-from-a-background-task-or-service
+//http://stackoverflow.com/questions/10630737/how-to-stop-a-thread-created-by-implementing-runnable-interface
+//android permission used:
+//android.permission.GET_TASKS
+
+
 package com.gueei.applocker;
 
 import java.io.BufferedReader;
@@ -5,16 +12,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -50,16 +66,16 @@ public class DetectorService extends Service {
                 mStartForeground.invoke(this, mStartForegroundArgs);
             } catch (InvocationTargetException e) {
                 // Should not happen.
-                Log.w("ApiDemos", "Unable to invoke startForeground", e);
+                //debug: log.w("Detector", "Unable to invoke startForeground", e);
             } catch (IllegalAccessException e) {
                 // Should not happen.
-                Log.w("ApiDemos", "Unable to invoke startForeground", e);
+                //debug: log.w("Detector", "Unable to invoke startForeground", e);
             }
             return;
         }
         
         // Fall back on the old API.
-        setForeground(true);
+        stopForeground(true);
         mNM.notify(id, notification);
     }
     
@@ -75,10 +91,10 @@ public class DetectorService extends Service {
                 mStopForeground.invoke(this, mStopForegroundArgs);
             } catch (InvocationTargetException e) {
                 // Should not happen.
-                Log.w("ApiDemos", "Unable to invoke stopForeground", e);
+                //debug: log.w("Detector", "Unable to invoke stopForeground", e);
             } catch (IllegalAccessException e) {
                 // Should not happen.
-                Log.w("ApiDemos", "Unable to invoke stopForeground", e);
+                //debug: log.w("Detector", "Unable to invoke stopForeground", e);
             }
             return;
         }
@@ -86,11 +102,12 @@ public class DetectorService extends Service {
         // Fall back on the old API.  Note to cancel BEFORE changing the
         // foreground state, since we could be killed at that point.
         mNM.cancel(id);
-        setForeground(false);
+        stopForeground(false);
     }
 	
     @Override
     public void onCreate() {
+    	//debug: log.i("Detector","Service.Oncreate");
     	initConstant();
         mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         try {
@@ -106,7 +123,7 @@ public class DetectorService extends Service {
     
 	@Override
     public void onDestroy() {
-    	
+		//debug: log.i("Detector","Service.Ondestroy");
     	mThread.interrupt();
     	
         // Make sure our notification is gone.
@@ -118,11 +135,14 @@ public class DetectorService extends Service {
     // method will not be called.
     @Override
     public void onStart(Intent intent, int startId) {
+    	//debug: log.i("Detector","Service.Onstart");
         handleCommand(intent);
+        
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+    	//debug: log.i("Detector","Service.OnStartCommand");
         handleCommand(intent);
         // We want this service to continue running until it is explicitly
         // stopped, so return sticky.
@@ -154,31 +174,33 @@ public class DetectorService extends Service {
     	if (mThread!=null)
     		mThread.interrupt();
     	
-    	mThread = new MonitorLogThread(new ActivityStartingHandler(this));
+    	mThread = new MonitorlogThread(new ActivityStartingHandler(this));
     	mThread.start();
     }
     
     private static Thread mThread;
-    
     private static boolean constantInited = false;
     private static Pattern ActivityNamePattern; 
-    private static String LogCatCommand;
-    private static String ClearLogCatCommand;
+    private static String logCatCommand;
+    private static String ClearlogCatCommand;
     
     private void initConstant() {
+    	//debug: log.i("Detector","Service.OninitConstant");
     	if (constantInited) return;
     	String pattern = getResources().getString(R.string.activity_name_pattern);
-    	Log.d("Detector", "pattern: " + pattern);
+    	//debug: log.d("Detector", "pattern: " + pattern);
     	ActivityNamePattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
-    	LogCatCommand = getResources().getString(R.string.logcat_command);
-    	ClearLogCatCommand = getResources().getString(R.string.logcat_clear_command);
+    	logCatCommand = getResources().getString(R.string.logcat_command);
+    	ClearlogCatCommand = getResources().getString(R.string.logcat_clear_command);
     }
 
 
     
-    private class MonitorLogThread extends Thread{
+    private class MonitorlogThread extends Thread{
+    	
     	ActivityStartingListener mListener;
-    	public MonitorLogThread(ActivityStartingListener listener){
+    	public MonitorlogThread(ActivityStartingListener listener){
+    		//debug: log.i("Detector","Monitor//debug: logThread");
     		mListener = listener;
     	}
     	
@@ -186,35 +208,95 @@ public class DetectorService extends Service {
     	
 		@Override
 		public void run() {
-			try {
+			
+			//debug: log.i("Detector","RUN!");
+			
+			while(!this.isInterrupted() ){
+				
+				try {
+		            Thread.sleep(100);
+		            ////debug: log.i("Detector","try!");
+		            //This is the code I use in my service to identify the current foreground application, its really easy:
+
+		      		ActivityManager am = (ActivityManager) getBaseContext().getSystemService(ACTIVITY_SERVICE);
+		      		// The first in the list of RunningTasks is always the foreground task.
+		      		RunningTaskInfo foregroundTaskInfo = am.getRunningTasks(1).get(0);
+		      		
+		      		//Thats it, then you can easily access details of the foreground app/activity:
+		      		String foregroundTaskPackageName = foregroundTaskInfo.topActivity.getPackageName();
+		      		PackageManager pm = getBaseContext().getPackageManager();
+		      		PackageInfo foregroundAppPackageInfo = null;
+		  			String foregroundTaskAppName = null;
+		  			String foregroundTaskActivityName = foregroundTaskInfo.topActivity.getShortClassName().toString();
+		      		try {
+		  				foregroundAppPackageInfo = pm.getPackageInfo(foregroundTaskPackageName, 0);
+		  				foregroundTaskAppName = foregroundAppPackageInfo.applicationInfo.loadLabel(pm).toString();
+		  				
+		  				//debug: log.i("Detector",foregroundTaskAppName);
+		  			} catch (NameNotFoundException e) {
+		  				// TODO Auto-generated catch block
+		  				e.printStackTrace();
+		  			}
+		  			
+		  			if (mListener!=null){
+		  				//mListener.onActivityStarting(foregroundAppPackageInfo.packageName,foregroundTaskAppName);
+		  				mListener.onActivityStarting(foregroundAppPackageInfo.packageName,foregroundTaskActivityName);
+		  			}
+		              
+		              
+		          } catch (InterruptedException e) {
+		              // good practice
+		              Thread.currentThread().interrupt();
+		              return;
+		          }
+			}
+			//debug: log.i("Detector","Outside RUN!, TRY");
+			/*try {
+				//debug: log.i("Detector","Inside RUN!, try");
 	    		Process process;
-	    		process = Runtime.getRuntime().exec(ClearLogCatCommand);
-				process = Runtime.getRuntime().exec(LogCatCommand);
+	    		process = Runtime.getRuntime().exec(Clear//debug: logCatCommand);
+				process = Runtime.getRuntime().exec(//debug: logCatCommand);
 				br = new BufferedReader(new InputStreamReader(process.getInputStream()));
 				String line;
 				// Check if it matches the pattern
-				while(((line=br.readLine()) != null) && !this.isInterrupted()){
-					
-					Log.d("Detector", line);
+				//while(((line=br.readLine()) != null) && !this.isInterrupted()){
+				while(true){// && !this.isInterrupted()){
+					//debug: log.i("Detector","Inside RUN!, WHILE");
+					line=br.readLine();
+					//debug: log.d("Detector", line);
 					
 					// Ignore launchers
-					if (line.contains("cat=[" + Intent.CATEGORY_HOME + "]")) continue;
 					
-					Matcher m = ActivityNamePattern.matcher(line);
-					if (!m.find()) continue;
-					if (m.groupCount()<2){
-						// Log.d("Detector", "Unknown problem while matching logcat output. Might be SDK version?");
+					if (line.contains("cat=[" + Intent.CATEGORY_HOME + "]")) {
+					
+						//debug: log.i("Detector","Inside RUN!, if line contains CAT");
 						continue;
 					}
 					
-					if (mListener!=null) mListener.onActivityStarting(m.group(1), m.group(2));
+					Matcher m = ActivityNamePattern.matcher(line);
+					if (!m.find()){
+						//debug: log.i("Detector","Inside RUN!, if !m.find");
+						continue;
+					}
+					if (m.groupCount()<2){
+						// //debug: log.d("Detector", "Unknown problem while matching //debug: logcat output. Might be SDK version?");
+						//debug: log.i("Detector","Inside RUN!, if m.groupCount()<2");
+						continue;
+					}
 					
-					Log.i("Detector", "Found activity launching: " + m.group(1) + "  /   " + m.group(2));
+					if (mListener!=null){
+						//debug: log.i("Detector","Inside RUN!, mListener!=null");
+						mListener.onActivityStarting(m.group(1), m.group(2));
+					}
+					
+					//debug: log.i("Detector","Inside RUN!, end of WHILE");
+					////debug: log.i("Detector", "Found activity launching: " + m.group(1) + "  /   " + m.group(2));
 				}
+				////debug: log.i("Detector","Outside RUN!, WHILE");
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
+			}*/
+			
 		}
-    }
-
+    }  
 }
